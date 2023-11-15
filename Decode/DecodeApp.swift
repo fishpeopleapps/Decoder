cd12627bf88d090ab8198cf6fce297b39c4b0cb2
Kimmy Brewer <127132791+fishpeopleapps@users.noreply.github.com>
2023-11-15 11:21:54 -0700

    Initial Commit

---
+++ Decode/DecodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecodeApp.swift
+//  Decode
+//
+//  Created by Kimberly Brewer on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DecodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
